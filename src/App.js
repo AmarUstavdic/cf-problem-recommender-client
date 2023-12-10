598733e5bf571f4b386b21886be53df03342d6e0
lilwizzz <amar.ustavdija@gmail.com>
2023-12-10 09:05:11 +0100

    testing redirecting using Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from "./components/Login";
 
+function Dashboard() {
+    return (
+        <>
+            {/* Content for the Dashboard */}
+            GG
+        </>
+    );
+}
+
+function NotFoundComponent() {
+    return (
+        <>
+            Component not found
+        </>
+    );
+}
+
 function App() {
-  return (
-    <Login/>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="*" element={<NotFoundComponent />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
