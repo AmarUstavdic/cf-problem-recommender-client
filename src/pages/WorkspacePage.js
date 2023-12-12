1f7c94fe26f8135b58ce0e6a01a33c1746bfe4e4
lilwizzz <amar.ustavdija@gmail.com>
2023-12-12 13:14:11 +0100

    working slider in workspace page, w/ editor

--- src/pages/WorkspacePage.js
+++ src/pages/WorkspacePage.js
@@ -1,61 +1,107 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import MonacoEditor from "react-monaco-editor";
 import "./WorkspacePage.css";
 
 function WorkspacePage() {
-    const [code, setCode] = useState("// Write your code here");
-    const [sliderPosition, setSliderPosition] = useState(50);
 
+    const [code, setCode] = useState("// Write your code here");
     const handleCodeChange = (newCode) => {
         setCode(newCode);
     };
 
-    const handleSliderMove = (e) => {
-        const container = document.querySelector(".workspace-container");
-        const newPosition = (e.clientX / container.clientWidth) * 100;
-        setSliderPosition(newPosition);
+
+    const dividerWidth = 0.5;
+    const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+    const [oneWidth, setOneWidth] = useState(50);
+    const [twoWidth, setTwoWidth] = useState(50);
+    const [isDragging, setIsDragging] = useState(false);
+
+    const handleResize = () => {
+        setViewportWidth(window.innerWidth);
     };
 
-    const handleSliderRelease = () => {
-        document.removeEventListener("mousemove", handleSliderMove);
-        document.removeEventListener("mouseup", handleSliderRelease);
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    const handleDragStart = (e) => {
+        setIsDragging(true);
     };
 
-    const handleSliderClick = (e) => {
-        document.addEventListener("mousemove", handleSliderMove);
-        document.addEventListener("mouseup", handleSliderRelease);
+    const handleDrag = (e) => {
+        if (isDragging) {
+            const containerWidth = viewportWidth * (oneWidth / 100);
+            const newOneWidth = (e.clientX / viewportWidth) * 100;
+            const newTwoWidth = 100 - newOneWidth;
+
+            // Ensure the divider doesn't go beyond certain limits
+            if (newOneWidth > 20 && newTwoWidth > 20) {
+                setOneWidth(newOneWidth);
+                setTwoWidth(newTwoWidth);
+            }
+        }
     };
+
+    const handleDragEnd = () => {
+        setIsDragging(false);
+    };
+
+    useEffect(() => {
+        window.addEventListener("mousemove", handleDrag);
+        window.addEventListener("mouseup", handleDragEnd);
+
+        return () => {
+            window.removeEventListener("mousemove", handleDrag);
+            window.removeEventListener("mouseup", handleDragEnd);
+        };
+    }, [isDragging]);
+
+    const dividerStyle = {
+        width: `${dividerWidth}%`,
+        height: `100vh`,
+        backgroundColor: "red",
+        cursor: "col-resize",
+    };
+
+    const divOneStyle = {
+        width: `${oneWidth}%`,
+        height: `100vh`,
+        backgroundColor: "green",
+        overflow: "hidden",
+    };
+
+    const containerStyle = {
+        display: "flex",
+        flexDirection: "row",
+
+    }
 
     return (
-        <div className="workspace-container">
-            <div className="problem-description-container">
-                {/* Problem Description */}
-                <div className="problem-description">
-                    {/* Your problem description content */}
-                </div>
+        <div style={containerStyle}>
+            <div className="container-1" style={divOneStyle}>
+                <h1>{viewportWidth}</h1>
             </div>
-
-            {/* Slider */}
             <div
-                className="slider"
-                style={{ left: `calc(${sliderPosition}% - 5px)` }}
-                onMouseDown={handleSliderClick}
+                className="divider"
+                style={dividerStyle}
+                onMouseDown={handleDragStart}
             ></div>
 
-            <div className="monaco-editor-container">
-                {/* Monaco Editor */}
-                <MonacoEditor
-                    width="100%"
-                    height="100%"
-                    language="javascript"
-                    theme="vs-dark"
-                    value={code}
-                    options={{
-                        selectOnLineNumbers: true,
-                    }}
-                    onChange={handleCodeChange}
-                />
-            </div>
+            <MonacoEditor
+                width={`${twoWidth}%`}
+                height="100vh"
+                language="cpp"
+                theme="vs-dark"
+                value={code}
+                options={{
+                    selectOnLineNumbers: true,
+                }}
+                onChange={handleCodeChange}
+            />
+
         </div>
     );
 }
