2429ced6c2bd09aa1601ddd778c64dce2758b1ca
lilwizzz <amar.ustavdija@gmail.com>
2023-12-12 06:04:00 +0100

    login component looks complete/refactored

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,64 +1,23 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-//import './Login.css';
-
-
-// TODO: Refactor this entire component, drop bootstrap, and make your own css instead
-
+import React from 'react';
+import './Login.css';
 
 function Login() {
-    const [handle, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleLogin = () => {
-        // Handle login logic here
-        console.log('Logging in with:', { handle, password });
-    };
-
     return (
-        <div className="container mt-5">
-            <div className="row justify-content-center">
-                <div className="col-md-6">
-                    <div className="card">
-                        <div className="card-header">Login</div>
-                        <div className="card-body">
-                            <form>
-                                <div className="mb-3">
-                                    <label htmlFor="username" className="form-label">
-                                        Handle
-                                    </label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="username"
-                                        value={handle}
-                                        onChange={(e) => setUsername(e.target.value)}
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="password" className="form-label">
-                                        Password
-                                    </label>
-                                    <input
-                                        type="password"
-                                        className="form-control"
-                                        id="password"
-                                        value={password}
-                                        onChange={(e) => setPassword(e.target.value)}
-                                    />
-                                </div>
-                                <button
-                                    type="button"
-                                    className="btn btn-primary"
-                                    onClick={handleLogin}
-                                >
-                                    Login
-                                </button>
-                            </form>
-                        </div>
-                    </div>
+        <div className="container">
+            <h1 className="title">Login</h1>
+            <form className="form-container">
+                <div className="form-group">
+                    <label htmlFor="username">Handle</label>
+                    <input type="text" id="username" name="username" />
                 </div>
-            </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" />
+                </div>
+                <button className="btn" type="submit">
+                    Login
+                </button>
+            </form>
         </div>
     );
 }
