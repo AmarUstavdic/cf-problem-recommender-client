f1e925038f6e3d61c1087877d928de8ce914ec04
lilwizzz <amar.ustavdija@gmail.com>
2023-12-12 08:51:17 +0100

    example of monaco editor usage working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from "./components/Login";
 import ProblemCard from "./components/ProblemCard";
+import RecommendedProblemsPage from "./pages/RecommendedProblemsPage";
 
 
 // TODO: Figure out how to do redirecting to different pages, and restrict access to pages if not authenticated
@@ -30,6 +31,7 @@ function App() {
                 <Route path="/" element={
                     <ProblemCard/>
                 } />
+                <Route path="/workspace" element={ <RecommendedProblemsPage/> } />
                 <Route path="/login" element={<Login />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="*" element={<NotFoundComponent />} />
--- src/pages/RecommendedProblemsPage.js
+++ src/pages/RecommendedProblemsPage.js
@@ -1,10 +1,34 @@
-import React from "react";
-
+import React, { useState } from "react";
+import MonacoEditor from "react-monaco-editor";
 
 function RecommendedProblemsPage() {
-    return(
-        <div>
+    const [code, setCode] = useState("// Write your code here");
 
+    const handleCodeChange = (newCode) => {
+        setCode(newCode);
+    };
+
+    return (
+        <div>
+            <h2>Recommended Problems</h2>
+
+            {/* Monaco Editor */}
+            <MonacoEditor
+                width="800"
+                height="600"
+                language="javascript"
+                theme="vs-dark"
+                value={code}
+                options={{
+                    selectOnLineNumbers: true,
+                }}
+                onChange={handleCodeChange}
+            />
+
+            {/* Additional Content */}
+            <div>
+                <p>Additional content goes here...</p>
+            </div>
         </div>
     );
 }
