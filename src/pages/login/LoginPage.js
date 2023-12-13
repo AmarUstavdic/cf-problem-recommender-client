51cc8053bd3939c0372bae63c2686015b4d95d02
lilwizzz <amar.ustavdija@gmail.com>
2023-12-13 20:06:56 +0100

    .

---
+++ src/pages/login/LoginPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Row, Col, Form, Button, Card } from 'react-bootstrap';
+import Login from "../../components/Login";
+import './LoginPage.css'; // Import your custom styles
+
+function LoginPage() {
+    return (
+        <></>
+    );
+}
+
+export default LoginPage;
